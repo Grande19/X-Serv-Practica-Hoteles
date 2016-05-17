1965d1daaa7b832c4eaa60d2bd573e6db2e1da5f
grande19 <1b15agc@gmail.com>
2016-05-17 23:21:45 +0200

    XML Parser

--- pr_final/Hoteles/xml_parser.py
+++ pr_final/Hoteles/xml_parser.py
@@ -1,23 +1,90 @@
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
 import sys
-from datetime import datetime
-from models import Hotel,User
 import string
 import urllib , urllib2
+import os.path
 
-class ConterHandler (ContentHandler) :
+class CounterHandler (ContentHandler) :
 
-    def __init(self):
-        self.inBasicData = False
-        self.inGeoData = False
-        self.inMultimedia = False
+    def __init__(self):
+        self.inItem = False
         self.inContent = False
         self.theContent = ""
-        self.extradata = False
-        self.inItem = False
+        self.hotellist = []
+        self.list = {}
+        self.cont = 1
+
+    def startElement (self , name , attrs):
+        if name == 'name': #empieza cuando encuentra la etiqueta name
+            self.inItem = True
+            self.inContent = True
+        if name == 'body' :
+            self.inItem = True
+            self.inContent = True
+        if name == 'web' :
+            self.inItem = True
+            self.inContent = True
+        if name == 'address' :
+            self.inItem = True
+            self.inContent = True
+        if name == 'media' and attrs["type"] == "image" :
+            self.inItem = True
+        elif self.inItem:
+            if name == "url" and self.cont<6 : #unicamente mostrar 5 imagenes
+                self.cont += self.cont + 1
+                self.inContent = True
+
+        if name == 'item' and attrs["name"] == "Subcategoria":
+            self.inItem = True
+            self.inContent = True
+
+    def endElement(self , name) :
+        if name == "name":
+            self.list[name] = self.theContent #aniadimos el contenido de dentro de la etiqueta al diccionario de hoteles
+            self.inItem = False
+            self.inContent = False
+        if name == "body" :
+            self.list[name] = self.theContent
+            self.inItem = False
+            self.inContent = False
+        if name == "web" :
+            self.list[name] = self.theContent
+            self.inItem = False
+            self.inContent = False
+        if name == "address" :
+            self.list[name] = self.theContent
+            self.inItem = False
+            self.inContent = False
+        if name == "item" :
+            self.list[name] = self.theContent
+            self.hotellist.append(self.list) #se aniade a la lista de hoteles
+            self.inItem = False
+            self.inContent = False
+        if name == "media":
+            self.inItem = False
+        elif self.inItem :
+            if name == "url" :
+                self.list[name] = self.theContent
+                self.inContent = False
+
+    def characters (self, chars):
+        if self.inContent:
+            self.theContent = chars
+
+    def dameLista(self):
+        return self.hotellist
 
 
-    def startElement (self , name , attr):
-        if name == 'service':
-            #self.service = normalize_whitespace(attrs.get)
+
+theParser = make_parser()
+theHandler = CounterHandler()
+theParser.setContentHandler(theHandler)
+
+# Ready, set, go!
+
+xmlFile = urllib.urlopen('http://www.esmadrid.com/opendata/alojamientos_v1_es.xml')
+theParser.parse(xmlFile)
+#return ('Actualizacion completada')
+
+print "Parse complete"
